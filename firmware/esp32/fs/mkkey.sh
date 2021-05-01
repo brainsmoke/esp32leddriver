951d7cb47609954bbeadf8c9cbeef6f438de444c
Erik Bosman <erik@minemu.org>
2021-05-01 15:02:50 +0200

    use ecdsa

--- firmware/esp32/fs/mkkey.sh
+++ firmware/esp32/fs/mkkey.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-openssl req -newkey rsa:2048 -nodes -keyout secret/key.pem -x509 -days 3650 -out secret/cert.pem -subj "/CN=esp32leddriver"
+ca_key=secret/ca_key.pem
+ca_cert=secret/ca_cert.pem
+csr=secret/csr.pem
+key=secret/key.pem
+cert=secret/cert.pem
+
+openssl ecparam -genkey -name secp256r1 -out "$ca_key"
+openssl req -x509 -new -SHA256 -nodes -key "$ca_key" -days 3650 -out "$ca_cert" -subj "/CN=esp32leddriver"
+openssl ecparam -genkey -name secp256r1 -out "$key"
+openssl req -new -SHA256 -key "$key" -nodes -out "$csr" -subj "/CN=esp32leddriver"
+openssl x509 -req -SHA256 -days 3650 -in "$csr" -CA "$ca_cert" -CAkey "$ca_key" -CAcreateserial -out "$cert"
