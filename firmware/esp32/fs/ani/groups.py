7d01146e3a29f6d0fb21b117107b8bcd6d3a5f6a
Erik Bosman <erik@minemu.org>
2021-05-05 11:18:51 +0200

    tweak faces 'animation'

--- firmware/esp32/fs/ani/groups.py
+++ firmware/esp32/fs/ani/groups.py
@@ -7,7 +7,7 @@ class Faces:
 
     def __init__(self, leds, config=None):
         assert 'faces' in leds.circuits
-        self.colors = [ (0xff,0xe6,0x9c), (0xff,0x99,23) ]
+        self.colors = [ (0x20,0x18,0x5), (0xff,0x99,23) ]
         self.group = bytearray(leds.n_leds)
         self.wave = get_smooth_wave()
 
@@ -26,8 +26,18 @@ class Faces:
         if config:
             config.add_slider('speed', 0, 20, .01, self.get_speed, self.set_speed, caption="speed")
 
+        self.set_min_fade(128)
+        if config:
+            config.add_slider('min_fade', 0, 257, 1, self.get_min_fade, self.set_min_fade, caption="fade minimum")
+
         self.phase = 0
         self.phase_max = 5*7*1000
+
+    def set_min_fade(self, min_fade):
+        self.min_fade = min_fade
+
+    def get_min_fade(self):
+        return self.min_fade
 
     def set_speed(self, speed):
         self.speed_db = speed
@@ -45,12 +55,16 @@ class Faces:
     def next_frame(self, fbuf):
         
         for a, m in ( (0,5), (1,7) ):
-            w = self.wave[ int(2048. * ( (self.phase*m)%self.phase_max / self.phase_max) ) ] + 255
-            for i,v in enumerate(self.group):
-                if v == a:
-                    fbuf[i*3  ] = (self.colors[v][0]*w) >> 9
-                    fbuf[i*3+1] = (self.colors[v][1]*w) >> 9
-                    fbuf[i*3+2] = (self.colors[v][2]*w) >> 9
+            w = self.wave[ int(2048. * ( (self.phase*m)%self.phase_max / self.phase_max) ) ]
+            w = self.min_fade * 255 + (257-self.min_fade) * w
+            r = (self.colors[a][0]*w)>>16
+            g = (self.colors[a][1]*w)>>16
+            b = (self.colors[a][2]*w)>>16
+            for i in range(len(self.group)):
+                if self.group[i] ==a:
+                    fbuf[i*3  ] = r
+                    fbuf[i*3+1] = g
+                    fbuf[i*3+2] = b
 
         self.phase = (self.phase+self.speed)%self.phase_max
 
