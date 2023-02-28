f06258f9d2f2ccef6a6ca734cd41b51ade14d5ef
Erik Bosman <erik@minemu.org>
2023-02-28 20:31:34 +0100

    add relay binaries

---
+++ firmware/pdk/relay/progloop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while read a; do
+	../easy-pdk-programmer-software/easypdkprog -p /dev/ttyACM0 -n PMS150C -v write relay-pdk13.ihx && \
+	../easy-pdk-programmer-software/easypdkprog -p /dev/ttyACM0 -n PMS150C read readoutx.hex && \
+	echo "only diff should (may) be the clock calibration value" && \
+	diff readout-pdk13.hex readoutx.hex
+done
