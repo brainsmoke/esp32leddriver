2a2a7b09390ec7d5ff8bb655ed7e20ee0ea9f511
Erik Bosman <erik@minemu.org>
2023-12-23 22:57:04 +0100

    modify upload script so that it'll only upload the right model files

--- firmware/esp32/tools/upload.sh
+++ firmware/esp32/tools/upload.sh
@@ -3,12 +3,22 @@
 
 kill_screen
 
-for dir in {ani,conf,secret,models}/ models/*;
+MODEL="greatcircles"
+
+if [ "x$1" != "x" ];
+then
+    MODEL="$1"
+fi
+
+echo "$MODEL"
+exit 1
+
+for dir in {ani,conf,secret,models}/ models/"$MODEL";
 do
 	[ -f "$dir" ] || do_echo ampy -p "$device" mkdir "${dir%/}"
 done
 
-for f in *.py {ani,conf}/* secret/httpd.json models/*/*;
+for f in *.py {ani,conf}/* secret/httpd.json models/"$MODEL"/*;
 do
 	[ -f "$f" ] && 	do_echo ampy -p "$device" put "$f" "/$f";
 done
