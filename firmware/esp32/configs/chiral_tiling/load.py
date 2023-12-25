20c063fd189ec5ef3ae86c0aa33d7e4753b90ff6
Erik Bosman <erik@minemu.org>
2023-12-25 01:23:37 +0100

    remove unused animations from configs

--- firmware/esp32/configs/chiral_tiling/load.py
+++ firmware/esp32/configs/chiral_tiling/load.py
@@ -4,8 +4,8 @@ from ani.lorenz import Lorenz
 from ani.fire import Fire
 from ani.gradient import Gradient, Spiral, Wobble
 from ani.spot import Spots, Chroma
-from ani.materials import Checkers, AlienPlanet
+from ani.materials import AlienPlanet
 
 def get_animations():
-	return (Lorenz, Fire, Gradient, Orbit, Wobble, Checkers, AlienPlanet, Spots, Chroma)
+	return (Lorenz, Fire, Gradient, Orbit, Wobble, AlienPlanet, Spots, Chroma)
 
--- firmware/esp32/configs/odsphere/load.py
+++ firmware/esp32/configs/odsphere/load.py
@@ -5,12 +5,12 @@ from conf.fire import Fire
 from ani.gradient import Gradient, Spiral, Wobble
 from ani.spot import Spots, Chroma
 from ani.rutherford import Rutherford
-from ani.materials import Checkers, AlienPlanet
+from ani.materials import AlienPlanet
 from ani.multiwave import MultiWave
 from ani.movie import Earth
 from ani.shadowplay import ShadowPlay
 from ani.shadowwalk import ShadowWalk
 
 def get_animations():
-	return (MultiWave, Lorenz, ShadowPlay, Gradient, Earth, ShadowWalk, Fire, Spiral, Orbit, Wobble, Checkers, AlienPlanet, Spots, Chroma)
+	return (MultiWave, Lorenz, ShadowPlay, Gradient, Earth, ShadowWalk, Fire, Spiral, Orbit, Wobble, AlienPlanet, Spots, Chroma)
 
