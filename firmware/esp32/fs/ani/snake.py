50a92f2ad5c02bd3c701e87db2ca4d28bc6d61a8
Erik Bosman <erik@minemu.org>
2023-12-27 03:37:41 +0100

    nicer snake

--- firmware/esp32/fs/ani/snake.py
+++ firmware/esp32/fs/ani/snake.py
@@ -1,26 +1,13 @@
 
 import uarray, random
 import cball
-import model, util
+import model
 
 MAGIC=b'\1RANDOMWALK'
-
-_wave = None
-def get_wave():
-    global _wave
-    if not _wave:
-        _wave = uarray.array('H', 0 for _ in range(400) )
-        bump = memoryview(_wave)
-        cball.wave_for_gradient_lut(_wave)
-    return _wave
-
 
 class Snake:
 
     def __init__(self, leds, config=None, **kwargs):
-
-        self.wave = util.get_smooth_wave()
-        self.inside = leds.inside
 
         f = model.open_file("randomwalk.bin", "rb")
         magic = f.read(11)
@@ -43,36 +30,31 @@ class Snake:
         n_read = f.readinto(self.turns)
         assert n_read == 2*n_choices*n_positions
 
+        self.who = bytearray(leds.n_leds)
+        self.max_snake_len = 7
+        self.snake_len = self.max_snake_len
         self.n_choices = n_choices
-        self.pos = [0,0]
+        self.pos = ( [0]*self.max_snake_len, [0]*self.max_snake_len )
         self.dir = [0,0]
+        self.who[0] = len(self.pos)*self.snake_len
 
-        self.color = [ cball.ColorDrift(512, 3) for _ in range(2) ]
+        self.color = [ cball.ColorDrift(128, 3) for _ in range(2) ]
         self.fb = uarray.array('H', (0 for _ in range( leds.n_leds * 3 ) ) )
+        self.bfb = uarray.array('H', (0 for _ in range( leds.n_leds * 3 ) ) )
         self.phase = 0
-        self.phase_max = len(self.wave)//2
+        self.phase_max = 2000
 
-        self.fade = 1
         self.set_speed(100)
         if config:
             config.add_slider('speed', 0, 200, 1, self.get_speed, self.set_speed, caption="speed")
-            config.add_slider('fade', .33, 3, .01, self.get_fade, self.set_fade, caption="fade")
 
     def set_speed(self, speed):
         self.speed = speed
-        steps_per_frame = speed/self.phase_max/self.fade
-        self.blend = 0.75**steps_per_frame
+        steps_per_frame = self.speed/self.phase_max
+        self.blend = 1-0.75**steps_per_frame
 
     def get_speed(self):
         return self.speed
-
-    def set_fade(self, fade):
-        self.fade = fade
-        steps_per_frame = self.speed/self.phase_max/self.fade
-        self.blend = 0.75**steps_per_frame
-
-    def get_fade(self):
-        return self.fade
 
     def random_step(self, x):
         weights = self.weights[self.dir[x]*self.n_choices:(self.dir[x]+1)*self.n_choices]
@@ -80,8 +62,8 @@ class Snake:
         c = random.randrange(0, total_weight)
         for i in range(len(weights)):
             if c < weights[i]:
-                self.dir[x] = self.turns[self.pos[x]*self.n_choices+i]
-                self.pos[x] = self.moves[self.pos[x]*self.n_choices+i]
+                self.dir[x] = self.turns[self.pos[x][0]*self.n_choices+i]
+                self.pos[x][0] = self.moves[self.pos[x][0]*self.n_choices+i]
                 return
             c -= weights[i]
         assert False
@@ -93,22 +75,31 @@ class Snake:
         phase = self.phase
         brightness = min(self.phase_max, self.phase)
 
+        bfb = self.bfb
         fb = self.fb
-        cball.array_set(out, 0)
-        cball.array_blend(fb, out, fb, self.blend)
+        cball.array_blend(bfb, bfb, fb, self.blend)
+        cball.array_copy(out, bfb)
 
-        for x in range(len(self.pos)):
-            p = self.pos[x]
-            r,g,b = self.color[x].next_color()
-            fb[p*3  ] = max(fb[p*3  ], (r*self.wave[phase])>>16)
-            fb[p*3+1] = max(fb[p*3+1], (g*self.wave[phase])>>16)
-            fb[p*3+2] = max(fb[p*3+2], (b*self.wave[phase])>>16)
-
-        cball.array_copy(out, fb)
-
+        snake_len = self.snake_len
         if self.phase >= self.phase_max:
             for x in range(len(self.pos)):
+                p = self.pos[x]
+                last_pos = p[snake_len-1]
+                self.who[last_pos] -= 1
+                if self.who[last_pos] == 0:
+                    self.who[last_pos] = 0
+                    fb[last_pos*3  ] = 0
+                    fb[last_pos*3+1] = 0
+                    fb[last_pos*3+2] = 0
+                for i in range(self.snake_len-1, 0, -1):
+                    p[i] = p[i-1]
                 self.random_step(x)
+                new_pos = p[0]
+                self.who[new_pos] += 1
+                r,g,b = self.color[x].next_color()
+                fb[new_pos*3  ] = r
+                fb[new_pos*3+1] = g
+                fb[new_pos*3+2] = b
 
         self.phase %= self.phase_max
 
