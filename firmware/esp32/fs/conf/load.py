a397a57e12386939c9b0e2720df6523bc86e9efa
Erik Bosman <erik@minemu.org>
2023-12-28 19:47:30 +0100

    greatcircles: add shadowwalk animation

--- firmware/esp32/fs/conf/load.py
+++ firmware/esp32/fs/conf/load.py
@@ -6,7 +6,8 @@ from ani.gradient import Gradient, Spiral, Wobble
 from ani.spot import Spots, Chroma
 from ani.rutherford import Rutherford
 from ani.materials import Checkers, AlienPlanet
+from ani.shadowwalk import ShadowWalk
 
 def get_animations():
-	return (Lorenz, Rutherford, Fire, Gradient, Orbit, Wobble, Checkers, AlienPlanet, Spots, Chroma)
+	return (Lorenz, Rutherford, Fire, Gradient, Orbit, ShadowWalk, Wobble, Checkers, AlienPlanet, Spots, Chroma)
 
