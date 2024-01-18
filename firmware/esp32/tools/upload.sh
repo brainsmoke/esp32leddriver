e28b83e7c09f855a0ba396974219a1bfb02d67e0
Erik Bosman <erik@minemu.org>
2024-01-19 00:08:37 +0100

    add webdir to upload script

--- firmware/esp32/tools/upload.sh
+++ firmware/esp32/tools/upload.sh
@@ -8,12 +8,12 @@ kill_screen
 
 try_echo pushd "$FS_DIR"/
 
-for dir in {ani,conf,secret,models}/ "$MODEL";
+for dir in {ani,conf,secret,webroot,webroot/css,models}/ "$MODEL";
 do
 	[ -f "$dir" ] || try_echo ampy -p "$device" mkdir --exists-okay "${dir%/}"
 done
 
-for f in *.py ani/* secret/httpd.json;
+for f in *.py ani/* secret/httpd.json webroot/css/*;
 do
 	[ -f "$f" ] && try_echo ampy -p "$device" put "$f" "/$f";
 done
