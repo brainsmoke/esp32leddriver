d68bb3b0df44f57149c890df725b673005154866
Erik Bosman <erik@minemu.org>
2024-06-11 19:46:55 +0200

    fix assertions

--- firmware/stm32/common/ws2812_dma.h
+++ firmware/stm32/common/ws2812_dma.h
@@ -15,7 +15,7 @@
 #define PIN_MASK (DMA_MASK)
 #endif
 
-#if PIN_MASK & DMA_MASK != PIN_MASK
+#if (PIN_MASK & DMA_MASK) != PIN_MASK
 #error "only pins 0..(" ##DMA_WIDTH "-1) supported"
 #endif
 
@@ -53,7 +53,7 @@ _Static_assert( FRAME_SIZE == sizeof(transposed_t)*N_VALUES_PER_STRIP, "FRAME_SI
 
 /* 8 transfers/byte means 1 byte/dma width */
 _Static_assert( DMA_WIDTH == sizeof(transposed_t), "size of transposed_t not right" );
-_Static_assert( 16 == MAX_STRIPS, "MAX_STRIPS != 16" );
+_Static_assert( DMA_WIDTH == MAX_STRIPS, "MAX_STRIPS != DMA_WIDTH" );
 
 void ws2812_dma_init(GPIO_TypeDef *gpio, transfer_t mask, int t0h, int t1h, int t_pulse);
 void ws2812_dma_start(volatile transfer_t buf[], uint32_t transfers);
