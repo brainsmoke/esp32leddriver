04d8da953b4b022aeccef3c698aa0c18ae8dff83
Erik Bosman <erik@minemu.org>
2025-01-06 10:18:34 +0100

    add animation to big sphere

--- firmware/esp32/conf/chiral_big/load.py
+++ firmware/esp32/conf/chiral_big/load.py
@@ -2,11 +2,11 @@
 from ani.orbit import Orbit
 from ani.lorenz import Lorenz
 from ani.fire import Fire
-from ani.gradient import Gradient, Spiral, Wobble
+from ani.gradient import WaveSelectGradient, Gradient, Spiral, Wobble
 from ani.spot import Spots, Chroma
 from ani.materials import AlienPlanet
 from ani.snake import Snake
 
 def get_animations():
-	return ((Lorenz, {}), (Fire, {}), (Snake, {'snake_len': 20, 'snake_count': 12, 'max_speed': 1600}), (Gradient, {}), (Orbit, {}), (Wobble, {}), (AlienPlanet, {}), (Spots, {}), (Chroma, {}))
+	return ((Lorenz, {}), (Fire, {}), (Snake, {'snake_len': 20, 'snake_count': 12, 'max_speed': 1600}), (WaveSelectGradient, {}), (Spiral, {}), (Orbit, {}), (Wobble, {}), (AlienPlanet, {}), (Spots, {}), (Chroma, {}))
 
