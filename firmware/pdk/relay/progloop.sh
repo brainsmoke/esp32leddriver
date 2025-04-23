5b715886d987a4515ec02a88eefdf22e2c4b0393
Erik Bosman <erik@minemu.org>
2025-04-23 22:47:12 +0200

    update prog script

--- firmware/pdk/relay/progloop.sh
+++ firmware/pdk/relay/progloop.sh
@@ -4,5 +4,5 @@ while read a; do
 	~/easy-pdk-programmer-software/easypdkprog -p /dev/ttyACM0 -n PMS150C -v write relay_newpcb-pdk13.ihx && \
 	~/easy-pdk-programmer-software/easypdkprog -p /dev/ttyACM0 -n PMS150C read readoutx.hex && \
 	echo "only diff should (may) be the clock calibration value" && \
-	diff readout-pdk13.hex readoutx.hex
+	diff relay_newpcb-pdk13_readout.hex readoutx.hex
 done
